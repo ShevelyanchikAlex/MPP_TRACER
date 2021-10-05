e6cb5ac2cd35d150ad1f67ce55d80d7eb1674c5c
sheva <shevelyanchik01@mail.ru>
2021-10-05 13:34:08 +0300

    Added UnitTests.

---
+++ UnitTests/TracerTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using ConsoleApp.UsageExample;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TracerLib.Tracer.Impl;
+using TracerLib.Tracer.Interfaces;
+
+
+namespace UnitTests
+{
+    [TestClass]
+    class TracerTest
+    {
+        private ITracer _tracer;
+        private Foo _foo;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _tracer = new TracerImpl();
+            _foo = new Foo(_tracer);
+        }
+
+        [TestMethod]
+        public void SingleThreadTest()
+        {
+            _foo.MyMethod();
+            Assert.Equals(1, _tracer.GetTraceResult().GetBenchmarksThread().Count);
+        }
+
+        [TestMethod]
+        public void ThreadsCountTest()
+        {
+            Thread thread1 = new Thread(_foo.MyMethod);
+            Thread thread2 = new Thread(_foo.MyMethod);
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Assert.AreEqual(4, _tracer.GetTraceResult().GetBenchmarksThread().Count);
+        }
+    }
+}
