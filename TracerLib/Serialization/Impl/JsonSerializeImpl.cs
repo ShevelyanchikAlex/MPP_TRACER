c854b4b1c6b86f7db09c3701e10092d917face3a
sheva <shevelyanchik01@mail.ru>
2021-10-05 13:33:22 +0300

    Added TraceLib.

---
+++ TracerLib/Serialization/Impl/JsonSerializeImpl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using TracerLib.Serialization.Interfaces;
+using TracerLib.Tracer.Impl;
+
+
+namespace TracerLib.Serialization.Impl
+{
+    public class JsonSerializeImpl : ISerialize
+    {
+        public string Serialize(BenchmarkResult benchmarkResult)
+        {
+            return JsonConvert.SerializeObject(new Dictionary<string, ICollection<BenchmarkThread>>
+                {{"threads", benchmarkResult.GetBenchmarksThread().Values}}, Formatting.Indented);
+        }
+    }
+}
---
+++ TracerLib/Tracer/Impl/BenchmarkResult.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Concurrent;
+
+namespace TracerLib.Tracer.Impl
+{
+    public class BenchmarkResult
+    {
+        private ConcurrentDictionary<int, BenchmarkThread> BenchmarksThread { get; }
+
+        public BenchmarkResult(ConcurrentDictionary<int, BenchmarkThread> benchmarksThread)
+        {
+            BenchmarksThread = benchmarksThread;
+        }
+
+        public ConcurrentDictionary<int, BenchmarkThread> GetBenchmarksThread()
+        {
+            return BenchmarksThread;
+        }
+
+        public BenchmarkThread GetBenchmarkThreadById(int id)
+        {
+            return  BenchmarksThread.GetOrAdd(id, new BenchmarkThread(id));
+        }
+    }
+}
